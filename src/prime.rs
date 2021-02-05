dcfa17532ab9a821bd1e4b217652560d1e8edb57
illumination-k <illumination.k.27@gmail.com>
2021-02-06 05:02:54 +0900

    fast eratosthenes

--- src/prime.rs
+++ src/prime.rs
@@ -65,31 +65,31 @@ pub fn prime_factorize<T: PrimInt+NumAssign>(mut n: T) -> Vec<(T, T)> {
 
 #[snippet("sieve_of_eratosthenes")]
 pub fn sieve_of_eratosthenes<T: NumCast>(n: T) -> Vec<usize> {
-    let n = n.to_usize().expect("cannot convert n to usize") + 1;
+    let n = n.to_usize().expect("cannot convert n to usize");
     if n < 2 {
         return vec![]
     }
 
-    let mut is_primes = vec![true; n];
+    let mut flags = vec![true; n / 2];
+    flags[0] = false;
 
-    is_primes[0] = false;
-    is_primes[1] = false;
+    let sqrt_x = (((n as f64).sqrt() + 0.1).ceil() + 0.5) as usize;
+    let sqrt_xi = sqrt_x / 2;
 
-    for i in 2..n {
-        if is_primes[i] {
-            let mut j = i + i;
-            while j < n {
-                is_primes[j] = false;
-                j += i
-            }
+    for i in 1..sqrt_xi {
+        if !flags[i] { continue; }
+        let p = 2 * i + 1;
+        let start = 2 * i * (i + 1);
+        for mult in (start..flags.len()).step_by(p) {
+            flags[mult] = false;
         }
     }
 
-    is_primes.iter()
-        .enumerate()
-        .filter(|(_i, flag)| **flag)
-        .map(|(i, _flag)| i)
-        .collect()
+    std::iter::once(2).chain(
+        flags.iter().enumerate()
+            .filter(|(_i, flag)| **flag)
+            .map(|(i, _flag)| 2 * i + 1)
+    ).collect()
 }
 
 #[snippet("osa_k")]
@@ -103,18 +103,20 @@ pub struct OsaK<T: PrimInt + std::hash::Hash + NumAssign > {
 #[snippet("osa_k_make_sieves")]
 fn _make_sieve<T: PrimInt>(mut maxu: usize) -> Vec<T> {
     maxu += 1;
-    let mut sieve: Vec<T> = (0..maxu).map(|x| T::from(x).unwrap()).collect();
+    let mut sieve: Vec<usize> = (0..maxu).collect();
 
     let mut i = 2;
     while i*i < maxu {
-        if sieve[i].to_usize().unwrap() < i { i+=1; continue; }
+        if sieve[i] < i { i += 1; continue; }
         for j in (i*i..maxu).step_by(i) {
-            if sieve[j].to_usize().unwrap() == j { sieve[j] = T::from(i).unwrap() }
+            if sieve[j] == j { sieve[j] = i }
         }
         i += 1;
     }
 
-    sieve
+    sieve.into_iter()
+        .filter_map(|x| T::from(x))
+        .collect()
 }
 
 #[snippet("osa_k_impl")]
@@ -122,7 +124,7 @@ impl<T: PrimInt + std::hash::Hash + NumAssign > OsaK<T> {
     /// O(maxloglog(max))
     /// construct osa-k from max size
     pub fn new(max: T) -> Self {
-        let maxu = max.to_usize().unwrap();
+        let maxu = max.to_usize().expect("cannot convert to usize");
         let sieve = _make_sieve(maxu);
 
         Self { sieve }
@@ -131,6 +133,7 @@ impl<T: PrimInt + std::hash::Hash + NumAssign > OsaK<T> {
     /// O(max(vec)loglog(max(vec)))
     /// construct osa-k from Vector
     pub fn from(vec: Vec<T>) -> Self {
+        assert!(vec.len() > 0);
         let max = vec.iter().max().unwrap();
         let maxu = max.to_usize().unwrap();
         let sieve = _make_sieve(maxu);
@@ -186,20 +189,17 @@ mod test {
 
     #[test]
     fn test_eratosthenes_1() {
-        let vec = sieve_of_eratosthenes(1);
-        assert_eq!(vec, vec![]);
-    }
+        let vec1 = sieve_of_eratosthenes(1);
+        assert_eq!(vec1, vec![]);
 
-    #[test]
-    fn test_eratosthenes_2() {
-        let vec = sieve_of_eratosthenes(2);
-        assert_eq!(vec, vec![2]);
-    }
+        let vec2 = sieve_of_eratosthenes(2);
+        assert_eq!(vec2, vec![2]);
 
-    #[test]
-    fn test_eratosthenes_20() {
-        let vec = sieve_of_eratosthenes(20);
-        assert_eq!(vec, vec![2, 3, 5, 7, 11, 13, 17, 19]);
+        let vec20 = sieve_of_eratosthenes(20);
+        assert_eq!(vec20, vec![2, 3, 5, 7, 11, 13, 17, 19]);
+
+        let vec100 = sieve_of_eratosthenes(100);
+        assert_eq!(vec100, vec![2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
     }
 
     #[test]
@@ -215,8 +215,12 @@ mod test {
         assert!(!osa_k.is_prime(0));
         assert!(!osa_k.is_prime(1));
         assert!(!osa_k.is_prime(12));
+        assert!(!osa_k.is_prime(14));
 
         // bound
         assert!(!osa_k.is_prime(18));
+
+        // !TODO
+        // factors
     }
 }
