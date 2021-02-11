b6b56b7cc120ec91350fe060f7399080355412c0
illumination-k <illumination.k.27@gmail.com>
2021-02-12 00:14:33 +0900

    update graph

--- src/graph.rs
+++ src/graph.rs
@@ -1,214 +1,270 @@
-// reference to https://www.forcia.com/blog/001409.html
+pub mod listgraph {
+    use num_traits::{NumCast, One, Zero};
+    use std::{collections::BinaryHeap, fmt, ops::{Index, IndexMut}, writeln};
+    #[derive(Debug, Clone, Copy, PartialEq)]
+    pub struct Edge<W> {
+        target: usize,
+        weight: W,
+    }
 
-use num_traits::{PrimInt, zero};
-
-#[derive(Debug, Clone)]
-pub struct AdjGraph<T: PrimInt> {
-    adj_mat: Vec<Vec<Option<T>>>,
-    next: Vec<Vec<usize>>,
-    size: usize,
-}
-
-impl<T: PrimInt> AdjGraph<T> {
-    pub fn new(adj_mat: Vec<Vec<Option<T>>>) -> Self {
-        let n = adj_mat.len();
-        let mut next: Vec<Vec<usize>> = vec![];
-
-        for _ in 0..n {
-            next.push((0..n).map(|j| j).collect())
+    impl<W> Edge<W> 
+        where W: One + Copy
+    {
+        pub fn new(target: usize, weight: W) -> Self {
+            Self { target, weight }
         }
 
-        Self {
-            adj_mat: adj_mat,
-            next: next,
-            size: n,
-        } 
-    }
-    pub fn warshal_floyd(&mut self) {
-        for k in 0..self.size {
-            for i in 0..self.size {
-                for j in 0..self.size {
-                    if let (Some(dik), Some(dkj)) = (self.adj_mat[i][k], self.adj_mat[k][j]) {
-                        if self.adj_mat[i][j].is_none() || self.adj_mat[i][j].unwrap() > dik + dkj {
-                            self.adj_mat[i][j] = Some(dik + dkj);
-                            self.next[i][j] = self.next[i][k]
-                        }
-                    }
-                } 
-            } 
+        pub fn new_unweighted(target: usize) -> Self {
+            Self { target, weight: W::one() }
+        }
+
+        pub fn target(&self) -> usize {
+            self.target
+        }
+
+        pub fn weight(&self) -> W {
+            self.weight
         }
     }
 
-    pub fn shortest_path(&self, start: usize, goal: usize) -> Option<(T, Vec<usize>)> {
-        if self.adj_mat[start][goal].is_none() {
-            return None;
+    impl<W> PartialOrd for Edge<W>
+        where W: PartialEq + PartialOrd
+    {
+        fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+            other.weight
+                .partial_cmp(&other.weight)
+        }
+    }
+
+    impl<W> Eq for Edge<W> where W: PartialEq + PartialOrd {}
+
+    impl<W> Ord for Edge<W>
+        where W: PartialOrd + PartialEq + PartialOrd
+    {
+        fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+            other.weight.partial_cmp(&self.weight).expect("No Nan").then_with(|| self.target.cmp(&other.target))
+        }
+    }
+
+    #[derive(Clone)]
+    pub struct ListGraph<W> {
+        graph: Vec<Vec<Edge<W>>>
+    }
+
+    impl<W> fmt::Debug for ListGraph<W>
+        where W: Copy + One + Clone + ToString
+    {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            writeln!(f, "")?;
+            for source in 0..self.len() {
+                let targets = &self[source];
+                let mut write_str = String::new();
+                write_str += &(source.to_string() + " -> ");
+                write_str += &targets.iter().map(|x| x.target().to_string()).collect::<Vec<String>>().join(" ");
+                writeln!(f, "{}", write_str)?
+            }
+            Ok(())        
+        }
+    }
+
+    #[derive(Debug, Clone)]
+    pub enum Direction {
+        DiGraph,
+        UnGraph
+    }
+
+    fn add_target<W: One + Copy>(
+        source: usize,
+        target: usize,
+        weight: W,
+        graph_type: &Direction,
+        v: &mut Vec<Vec<Edge<W>>>
+    ) {
+        match graph_type {
+            Direction::DiGraph => {
+                v[source].push(Edge::new(target, weight))
+            },
+            Direction::UnGraph => {
+                v[source].push(Edge::new(target, weight));
+                v[target].push(Edge::new(source, weight))
+            }
+        }
+    }
+
+    impl<W> ListGraph<W>
+        where W: Clone + One + Copy
+    {
+        pub fn new(n: usize) -> Self {
+            Self {
+                graph: vec![vec![]; n]
+            }
         }
 
-        let mut path = vec![start];
-        let mut node = start;
+        pub fn unweighted_from(vec: Vec<(usize, usize)>, offset: usize, graph_type: Direction) -> Self {
+            let max_val = *vec.iter().map(|(i, j)| std::cmp::max(i, j)).max().unwrap() + 1 - offset;
+            let mut graph = vec![vec![]; max_val];
+            for &(a, b) in vec.iter() {
+                add_target(a-offset, b-offset, W::one(), &graph_type, &mut graph)
+            }
 
-        while node != goal {
-            path.push(self.next[node][goal]);
-            node = self.next[node][goal];
+            Self {
+                graph
+            }
         }
-        Some((self.adj_mat[start][goal].unwrap(), path))
+
+        pub fn weighted_from(vec: Vec<(usize, usize, W)>, offset: usize, graph_type: Direction) -> Self {
+            let max_val = *vec.iter().map(|(i, j, _w)| std::cmp::max(i, j)).max().unwrap() + 1 - offset;
+            let mut graph = vec![vec![]; max_val];
+            for &(a, b, w) in vec.iter() {
+                add_target(a-offset, b-offset, w, &graph_type, &mut graph)
+            }
+
+            Self {
+                graph
+            }
+        }
+
+        pub fn len(&self) -> usize {
+            self.graph.len()
+        }
+    }
+
+    impl<W> Index<usize> for ListGraph<W> {
+        type Output = Vec<Edge<W>>;
+
+        fn index(&self, index: usize) -> &Self::Output {
+            &self.graph[index]
+        }
+    }
+
+    impl<W> IndexMut<usize> for ListGraph<W> {
+        fn index_mut(&mut self, index: usize) -> &mut Vec<Edge<W>> {
+            &mut self.graph[index]
+        }
+    }
+
+    pub type UnweightedListGraph = ListGraph<usize>;
+
+    pub struct NeighborhoodIter<'a, W> {
+        source: usize,
+        counter: usize,
+        graph: &'a ListGraph<W>,
+    }
+
+    pub struct NeighborhoodUnweightedIter<'a, W> {
+        source: usize,
+        counter: usize,
+        graph: &'a ListGraph<W>,
+    }
+
+
+    impl<'a, W> Iterator for NeighborhoodIter<'a, W> {
+        type Item = &'a Edge<W>;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            self.counter += 1;
+            self.graph[self.source].get(self.counter - 1)
+        }
+    }
+
+    impl <'a, W> Iterator for NeighborhoodUnweightedIter<'a, W>
+        where W: Copy + One
+    {
+        type Item = &'a usize;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            if self.counter >= self.graph[self.source].len() {
+                return None
+            }
+            self.counter += 1;
+            Some(&(self.graph[self.source][self.counter - 1].target))
+        }
+    }
+
+    pub trait NeighborhoodExt<W> {
+        fn neighbors(&self, source: usize) -> NeighborhoodIter<W>;
+        fn neighbors_unweighted(&self, source: usize) -> NeighborhoodUnweightedIter<W>;
+    }
+
+    impl<W> NeighborhoodExt<W> for ListGraph<W> {
+        fn neighbors(&self, source: usize) -> NeighborhoodIter<W> {
+            NeighborhoodIter {
+                source,
+                counter: 0,
+                graph: self
+            }
+        }
+
+        fn neighbors_unweighted(&self, source: usize) -> NeighborhoodUnweightedIter<W> {
+            NeighborhoodUnweightedIter {
+                source,
+                counter: 0,
+                graph: self
+            }
+        }
+    }
+
+    pub fn diktstra<W>(graph: &ListGraph<W>, start: usize) -> (Vec<W>, Vec<usize>) 
+        where W: Copy + One + Zero + PartialEq + PartialOrd + NumCast
+    {
+        let mut dist = vec![W::from(1<<50).unwrap(); graph.len()];
+        dist[start] = W::zero();
+
+        let mut bq = BinaryHeap::new();
+
+        let mut prev_nodes = vec![std::usize::MAX; graph.len()];
+
+        // add dumy edge and start position
+        bq.push((Edge::new(0, W::one()), start));
+
+        while let Some((edge, position)) = bq.pop() {
+            for &e in graph.neighbors(position) {
+                if edge.weight() > dist[position] { continue; }
+                bq.push((e, e.target()));
+                dist[edge.target()] = dist[position] + edge.weight();
+                prev_nodes[edge.target()] = position;
+            }
+        }
+
+        (dist, prev_nodes)
+    }
+
+    pub fn restore_path(start: usize, goal: usize, prev_nodes: &Vec<usize>) -> Vec<usize> {
+        let mut res = vec![goal];
+        let mut pos = start;
+        while pos != start {
+            pos = prev_nodes[pos];
+            res.push(pos)
+        }
+        res
     }
 }
 
 #[cfg(test)]
 mod test {
-    use super::AdjGraph;
+    use super::listgraph::*;
     #[test]
-    fn test_warshall_floyd() {
-        let adj_mat = vec![
-            vec![None, Some(1), Some(10), None, None],
-            vec![None, None, None, Some(2), None],
-            vec![None, Some(1), None, Some(3), Some(1)],
-            vec![Some(7), None, None, None, Some(2)],
-            vec![None, None, None, None, None],
-        ];
-
-        let mut graph = AdjGraph::new(adj_mat);
-        graph.warshal_floyd();
-
-        assert_eq!(graph.shortest_path(0, 1), Some((1, vec![0, 1])));
-        assert_eq!(graph.shortest_path(0, 3), Some((3, vec![0, 1, 3])));
-        assert_eq!(graph.shortest_path(3, 0), Some((7, vec![3, 0])));
-        assert_eq!(graph.shortest_path(0, 4), Some((5, vec![0, 1, 3, 4])));
-        assert_eq!(graph.shortest_path(4, 0), None);
+    fn test_fmt() {
+        let graph: ListGraph<usize> = ListGraph::unweighted_from(vec![(0, 1), (1, 2), (4, 1), (1, 3)], 0, Direction::UnGraph);
+        dbg!(&graph);
     }
-}
 
-#[derive(Debug, Clone, Copy, Eq, PartialEq)]
-pub struct Edge<T: PrimInt> {
-    node: T,
-    cost: T,
-}
-
-// for BinaryHeap
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-struct State<T: PrimInt> {
-    cost: T,
-    position: usize,
-    pre_node: usize,
-}
-
-impl<T: PrimInt> Ord for State<T> {
-    fn cmp(&self, other: &State<T>) -> std::cmp::Ordering {
-        other
-            .cost
-            .cmp(&self.cost)
-            .then_with(|| self.position.cmp(&other.position))
-    }
-}
-
-impl<T: PrimInt> PartialOrd for State<T> {
-    fn partial_cmp(&self, other: &State<T>) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct ListGraph<T: PrimInt> {
-    list_graph: Vec<Vec<Edge<T>>>,
-    dist: Vec<usize>,
-    pre_nodes: Vec<usize>,
-    heap: std::collections::BinaryHeap<State<T>>,
-    size: usize,
-}
-
-impl<T: PrimInt> ListGraph<T> {
-    pub fn new(list_graph: Vec<Vec<Edge<T>>>) -> Self {
-        let size = list_graph.len();
-        let dist = (0..size).map(|_| std::usize::MAX).collect();
-        let pre_nodes = (0..size).map(|i| i).collect();
-        let heap: std::collections::BinaryHeap<State<T>> = std::collections::BinaryHeap::new();
-        Self { list_graph, dist, pre_nodes, heap, size } }
-        
-    fn refresh(&mut self) {
-        self.dist = (0..self.size).map(|_| std::usize::MAX).collect();
-        self.pre_nodes = (0..self.size).map(|i| i).collect();
-        self.heap = std::collections::BinaryHeap::new();
-    }
-}
-
-
-impl<T: PrimInt> ListGraph<T> {
-    fn _dijkstra(&mut self, start: usize, goal: usize) -> Option<(T, Vec<usize>)> {
-        self.dist[start] = 0;
-        self.heap.push(
-            State {
-                cost: zero(),
-                position: start,
-                pre_node: 0,
-            }
-        );
-
-        while let Some(State {
-            cost, position, pre_node,
-        }) = self.heap.pop() {
-            if cost.to_usize().unwrap() > self.dist[position] { continue; }
-            self.pre_nodes[position] = pre_node;
-
-            if position == goal {
-                let mut v = goal;
-                let mut path = vec![goal];
-                while v != start {
-                    path.push(self.pre_nodes[v]);
-                    v = self.pre_nodes[v];
-                }
-                path.reverse();
-                return Some((cost, path));
-            }
-
-            for edge in self.list_graph[position].iter() {
-                let next = State {
-                    cost: cost + edge.cost,
-                    position: edge.node.to_usize().unwrap(),
-                    pre_node: position,
-                };
-
-                if next.cost.to_usize().unwrap() < self.dist[next.position] {
-                    self.heap.push(next);
-                    self.dist[next.position] = next.cost.to_usize().unwrap();
-                }
-            }
+    #[test]
+    fn test_iter() {
+        let graph: ListGraph<usize> = ListGraph::unweighted_from(vec![(0, 1), (1, 2), (4, 1), (1, 3)], 0, Direction::UnGraph);
+        let mut v = vec![];
+        for n in graph.neighbors(1) {
+            v.push(n.target());
         }
-        None
+        assert_eq!(v, vec![0, 2, 4, 3])
     }
-
-    pub fn dijkstra(&mut self, start: usize, goal: usize) -> Option<(T, Vec<usize>)> {
-        let ret = self._dijkstra(start, goal);
-        self.refresh();
-        ret
-    }
-}
-
-
-#[cfg(test)]
-mod test_list_graph {
-    use super::{Edge, ListGraph};
     #[test]
-    fn test_dijktstra_1() {
-        let list_graph = vec![
-            vec![Edge { node: 2, cost: 10 }, Edge { node: 1, cost: 1 }],
-            vec![Edge { node: 3, cost: 2 }],
-            vec![
-                Edge { node: 1, cost: 1 },
-                Edge { node: 3, cost: 3 },
-                Edge { node: 4, cost: 1 },
-            ],
-            vec![Edge { node: 0, cost: 7 }, Edge { node: 4, cost: 2 }],
-            vec![],
-        ];
-
-        let mut graph = ListGraph::new(list_graph);
-        assert_eq!(graph.dijkstra(0, 1), Some((1, vec![0, 1])));
-        assert_eq!(graph.dijkstra(0, 3), Some((3, vec![0, 1, 3])));
-        assert_eq!(graph.dijkstra(3, 0), Some((7, vec![3, 0])));
-        assert_eq!(graph.dijkstra(0, 4), Some((5, vec![0, 1, 3, 4])));
-        assert_eq!(graph.dijkstra(4, 0), None);
+    fn test_iter_unweighted() {
+        let graph: ListGraph<usize> = ListGraph::unweighted_from(vec![(0, 1), (1, 2), (4, 1), (1, 3)], 0, Direction::UnGraph);
+        let mut v = vec![];
+        for &n in graph.neighbors_unweighted(1) {
+            v.push(n);
+        }
+        assert_eq!(v, vec![0, 2, 4, 3])
     }
 }
-
-
--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,11 @@ extern crate cargo_snippet;
 pub mod dp;
 pub mod mod_int;
 pub mod prime;
-pub mod graph;
 pub mod graph2d;
+pub mod graph;
 pub mod calc_comb;
 pub mod math;
 pub mod scanner;
 pub mod data_structures;
+
+pub mod viz;
