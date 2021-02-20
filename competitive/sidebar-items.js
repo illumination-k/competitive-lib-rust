2924b47253821329bdcbd1c12f0c25b664f336d3
illumination-k <illumination-k@users.noreply.github.com>
2021-02-20 16:32:12 +0000

    deploy: fd93b3379c3a737ff25796e1be59c0d49526878a

--- competitive/sidebar-items.js
+++ competitive/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["combinations",""],["data_structures",""],["dp",""],["geometry",""],["graph",""],["graph2d",""],["math",""],["mod_int",""],["prime",""],["scanner",""],["test_utility",""]]});
+initSidebarItems({"mod":[["combinations",""],["data_structures",""],["dp",""],["format",""],["geometry",""],["graph",""],["graph2d",""],["math",""],["mod_int",""],["prime",""],["scanner",""],["test_utility",""]]});
